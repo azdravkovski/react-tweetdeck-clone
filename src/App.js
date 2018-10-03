f255233a531567746723901d1a2c976b5befd3dd
Aleksandar Zdravkovski <24496229+azdravkovski@users.noreply.github.com>
2018-10-03 10:18:22 +0200

    feat: Added Posts deck

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +6,79 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>TweetDeck Clone</h1>
         </header>
+        <MainWrapper />
       </div>
     );
   }
+}
+
+class MainWrapper extends Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+        <PostDeck />
+      </div>
+    )
+  }
+}
+
+class PostDeck extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPosts()
+  }
+
+  fetchPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(data => data.json())
+      .then(posts => {
+        let postArr = posts.map((post, i) => {
+          while (i < 10) {
+            return <Post title={post.title} body={post.body} />
+          }
+        })
+        this.setState({
+          posts: postArr
+        })
+      }
+      )
+  }
+
+  render() {
+    return (
+      <div className="post-deck">
+        <PostDeckHeader />
+        {this.state.posts}
+      </div>
+    )
+  }
+}
+
+const Post = (props) => {
+  return (
+    <div className="post">
+      <h3 className="post-title">{props.title}</h3>
+      <p className="post-body">{props.body}</p>
+    </div>
+  )
+}
+
+const PostDeckHeader = (props) => {
+  return (
+    <div className="post-deck-header">
+      <h2>Posts</h2>
+      <input type="text" placeholder="Search..." />
+    </div>
+  )
 }
 
 export default App;
